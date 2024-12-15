2ab9f323536c2b6ff62f8d9b071e9a4bc7cf151d
loqpw <hassooonali@gmail.com>
2024-12-15 04:59:50 +0300

    ad

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php
-echo 'balls';
+$v = 'mal';
+var_dump($v);
+$x = 10.23;
+echo "<br>asdasd $v <br>";
+echo "<br>asdasd $x <br>";
+var_dump($x);
+$b=2;
+$a=3;
+function test(){
+    $GLOBALS['b']=$GLOBALS['a']*$GLOBALS['b'];
+    echo"<br>";
+    $a=$GLOBALS['a']+$GLOBALS['b'];
+    echo $a;
+    echo "<br>";
+    echo $GLOBALS['b'];
+}
+$c=strlen($v);
+test();
+echo"$c<br>";
+echo (strlen($v));
+$g=3;
+if($g==strlen($v)){
+    echo "<br>true";
+}
+if($g===strlen($v)){
+    echo "<br>true";
+}
+$trr="www";
+$asd="qwe";
+echo "<br>$trr $asd<br>";
+echo ($trr.=$asd);
+echo "<br>";
+$we="ffe";
+echo ($asd.$we);
 ?>
